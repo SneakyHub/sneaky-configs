7b6d72c4f94aa61b948014ad5d9baa1bb5ce7ff4
Sneaky <78713939+SneakyHub@users.noreply.github.com>
2022-05-02 09:53:07 +0100

    Update pterodactyl-install.sh

--- pterodactyl-install.sh
+++ pterodactyl-install.sh
@@ -1,75 +1,135 @@
+echo ""
 echo "Downloading Dependencies!"
+echo ""
 sudo apt install -y sudo software-properties-common curl apt-transport-https ca-certificates mariadb-common mariadb-server mariadb-client
+echo ""
 echo "Done!"
+echo ""
+echo ""
 echo "Updating and Upgrading!"
+echo ""
 sudo apt update -y && sudo apt upgrade -y
+echo ""
 echo "Done!"
+echo ""
+echo ""
 echo "Starting MariaDB"
+echo ""
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
+echo ""
 echo "Done!"
+echo ""
+echo ""
 echo "Installing php packages!"
-sudo curl https://packages.sury.org/php/apt.gpg -o /etc/apt/trusted.gpg.d/php.gpg
-sudo echo "deb https://packages.sury.org/php/ buster main" | tee /etc/apt/sources.list.d/php.list
+echo ""
+curl https://packages.sury.org/php/apt.gpg -o /etc/apt/trusted.gpg.d/php.gpg
+echo "deb https://packages.sury.org/php/ buster main" | tee /etc/apt/sources.list.d/php.list
+echo ""
+echo ""
 echo "Done!"
+echo ""
+echo ""
 echo "Updating and Upgrading!"
+echo ""
 sudo apt update -y && sudo apt upgrade -y
+echo ""
 echo "Done!"
+echo ""
+echo ""
 echo "Installing Php, Nginx and extensions!"
+echo ""
 sudo apt install -y nginx php8.0 php8.0-{cli,common,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip}
+echo ""
 echo "Done!"
+echo ""
+echo ""
 echo "Installing redis server"
+echo ""
 sudo apt install -y redis-server
+echo ""
 echo "Done!"
+echo ""
 echo "Starting Redis Server"
+echo ""
 sudo systemctl start redis-server
 sudo systemctl enable redis-server
+echo ""
 echo "Done!"
+echo ""
+echo ""
 echo "Installing Composer"
+echo ""
 sudo curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+echo ""
 echo "Done!"
+echo ""
+echo ""
 echo "Setting up MySQL - Grab a long random password"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
 read -p "Press enter to continue with MariaDB installation - Make sure you have the password!"
 sudo mysql_secure_installation
+echo ""
 echo "good job, now lets enable php fpm"
+echo ""
 sudo systemctl enable php8.0-fpm
 sudo systemctl start php8.0-fpm
+echo ""
 echo "Done!"
+echo ""
+echo ""
 echo "Lets make the pterodactyl directory and download web server files!"
+echo ""
 sudo mkdir -p /var/www/pterodactyl
 cd /var/www/pterodactyl
 sudo curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
+echo ""
 echo "Done!"
+echo ""
 echo "Okay, lets extract the panel files and install them!"
+echo ""
 sudo tar -xzvf panel.tar.gz
 sudo chmod -R 755 storage/* bootstrap/cache/
 sudo cp .env.example .env
 sudo composer install --no-dev --optimize-autoloader
+echo ""
 echo "Done!"
+echo ""
 sudo php artisan key:generate --force
-
+echo ""
 echo "Setting Pterodactyl web server permissions!"
+echo ""
 sudo chown -R www-data:www-data /var/www/pterodactyl/*
+echo ""
 echo "Done!"
-
+echo ""
+echo ""
 echo "Downloading Pteroq service from SneakyHub github"
+echo ""
 cd /etc/systemd/system
 
 wget -O pteroq.service https://raw.githubusercontent.com/SneakyHub/sneaky-configs/main/pteroq.service
+echo ""
 echo "Done!"
+echo ""
 
 cd /var/www/pterodactyl
-
+echo ""
 echo "Enabling Systemctl Redis & Pteroq services"
+echo ""
 sudo systemctl enable --now redis-server
-
 sudo systemctl enable --now pteroq.service
+echo ""
 echo "Done!"
-
 echo ""
 echo ""
 echo ""
-
+echo ""
+echo ""
 echo "you need to setup the following... "
 echo "Command: php artisan p:environment:setup"
 echo "Command: php artisan p:environment:database"
